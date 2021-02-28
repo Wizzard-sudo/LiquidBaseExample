48d02a97bfe2aaa97c93ecae8c8f1ed8da411e0b
Wizzard-sudo <kolia.makarevich.98@gmail.com>
2021-02-28 13:21:05 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/LiquidBaseExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquidBaseExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquidBaseExampleApplication.class, args);
+	}
+
+}
